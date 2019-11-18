b0f619e36f8f9ecb862131a915d9cb1f4e666dc7
Alex Cuba <alex.cuba@stonybrook.edu>
2019-11-18 18:18:04 -0500

    Working Move Up, Down, and Delete Item

--- src/components/list_screen/ItemCard.js
+++ src/components/list_screen/ItemCard.js
@@ -3,6 +3,7 @@ import {Button, Icon} from 'react-materialize';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
+import { updateItem } from '../../store/actions/actionCreators.js';
 
 class ItemCard extends React.Component {
 
@@ -30,6 +31,37 @@ class ItemCard extends React.Component {
         }
     }
 
+    moveItemUp = (updateList, index) => {
+        if(index > 0){
+            let temp = updateList.items[index-1];
+            updateList.items[index-1] = updateList.items[index];
+            updateList.items[index-1].key -= 1;
+            updateList.items[index] = temp;
+            updateList.items[index].key +=1;
+        }
+        this.props.updateItem(updateList, updateList.id)
+    }
+
+    moveItemDown = (updateList, index) => {
+        if(index < updateList.items.length-1){
+          let temp = updateList.items[index+1];
+          updateList.items[index+1] = updateList.items[index];
+          updateList.items[index+1].key += 1;
+          updateList.items[index] = temp;
+          updateList.items[index].key -=1;
+          }
+        this.props.updateItem(updateList, updateList.id)
+    }
+
+    removeItem  = (updateList, index) => {
+        updateList.items.splice(index, 1);
+        let i = 0;
+        for(i; i < updateList.items.length; i++){
+            updateList.items[i].key = i;
+        }
+        this.props.updateItem(updateList, updateList.id)
+    }
+
     render() {
         const { item } = this.props;  
         const todoList = this.props.todoList;
@@ -43,9 +75,9 @@ class ItemCard extends React.Component {
                     <span className="card-title col s2">{this.ifCompleted(item.completed)}</span>
                     <div className="fixed-action-btn">
                     <Button fab={{direction:"left"}} floating large className="grey darken-1" onClick={(e) => e.preventDefault()}>
-                        <Button floating small className={this.upDisable(item)} waves="light"><Icon>arrow_upward</Icon></Button>
-                        <Button floating small className={this.downDisable(item, todoList)} waves="light"><Icon>arrow_downward</Icon></Button>
-                        <Button floating small className="black" waves="light"><Icon>delete</Icon></Button>
+                        <Button floating small className={this.upDisable(item)} waves="light" onClick={(e) => this.moveItemUp(todoList, item.key)}><Icon>arrow_upward</Icon></Button>
+                        <Button floating small className={this.downDisable(item, todoList)} waves="light" onClick={(e) => this.moveItemDown(todoList, item.key)}><Icon>arrow_downward</Icon></Button>
+                        <Button floating small className="black" waves="light" onClick={(e) => this.removeItem(todoList, item.key)}><Icon>delete</Icon></Button>
                     </Button>
                     </div>
                     </div>
@@ -63,8 +95,14 @@ const mapStateToProps = (state, ownProps) => {
     };
 };
 
+const mapDispatchtoProps = (dispatch) => {
+    return{
+    updateItem: (todoList, id) => dispatch(updateItem(todoList, id))
+    }
+}
+
 export default compose(
-    connect(mapStateToProps),
+    connect(mapStateToProps, mapDispatchtoProps),
     firestoreConnect([
         { collection: 'todoLists' },
     ]),
